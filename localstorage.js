83a1b8f479b382b96076a55132825038d565e4bd
Kirishima <97999610+water2thewave@users.noreply.github.com>
2022-03-11 19:18:24 -0500

    delete localstorage button

--- localstorage.js
+++ localstorage.js
@@ -1,7 +1,16 @@
 $(".delete-localstorage").on("click", deleteLocalStorage)
+$(".edit-json").bind("input propertychange", () => {
+  // when we edit json, attempt to load it in node
+
+});
 
 const localStorage = window.localStorage;
 
 function deleteLocalStorage() {
-  localStorage.clear();
+  window.localStorage.clear();
+  console.log('Localstorage deleted');
+}
+
+function onJsonChange(e) {
+
 }
