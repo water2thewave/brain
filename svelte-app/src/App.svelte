8eb2328416dd537fe1d3c0cb57b5cb77e8dfe0e3
Kirishima <97999610+water2thewave@users.noreply.github.com>
2022-03-13 03:22:46 -0400

    work

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -6,22 +6,59 @@
 
 	let editMode = false;
 	let role = 'wizard';
+	let	roles = [];
+	
+	getRoles();
+
+	function getRoles() {
+		let localStorage = window.localStorage;
+		for ( var i = 0, len = localStorage.length; i < len; ++i ) {
+			// let name = localStorage.getItem( localStorage.key(i) ) ;
+			roles.push(localStorage.key(i));
+		}
+	}
+function saveRole(role, data) {
+	const {nodes, links} = data;
+	console.log({data});
+
+	const saveLinks = links.map((link) => {
+		let {index, source, target} = link;
+		console.log({link});
+		if (source.id) {
+			let ret = {source: source.id, target: target.id};
+			return ret;
+		}
+		else {
+			return link;
+		}
+	});
+	// const saveNodes = nodes.map(({id, word}) =>  {id, word});
+	// const saveData = {nodes: saveNodes, links: saveLinks};
+	const saveData = {nodes, links: saveLinks};
+	console.log('saved to localStorage', saveData);
+	let jsonStr = JSON.stringify(saveData);
+	window.localStorage.setItem(role, jsonStr);
+}
+
 	let roleData = {nodes: [], links: []};
+	var selectedRole;
 
-	loadLocalStorage(role);
+	loadLocalStorage(role)
 
-	// $: saveToBrowser(roleData.nodes, roleData.links);
-
-	if (roleData) {
+	if (!selectedRole) {
 	// console.error(`Invalid json in localstorage for role "${role}"`, roleData);
 		console.log('Loading default data');
 		d3.json('data.json')
 			.then ((obj) => { 
 				roleData = obj;
 				console.log('default data loaded', roleData);
+				saveRole(role, obj);
+				selectedRole = role;
 			})
 			.catch(console.error);
 	}
+
+	$: saveRole(role, roleData);
 
 	function deleteLocalStorage() {
 		window.localStorage.clear();
@@ -30,11 +67,15 @@
 
 	function loadLocalStorage(role) {
 		let storedData = window.localStorage.getItem(role);
-		console.log(`Should class to ${role}`, storedData);
 		if (storedData != null) {
-			console.log(`Changing class to ${role}`, storedData);
-			roleData = storedData;
+			console.log(`Changing class to ${role}`);
+			roleData = JSON.parse(storedData);
+			selectedRole = role;
 		}
+		else {
+			console.log(`Did not class to ${role}`, roleData);
+		}
+
 	}
 
 	function saveToBrowser(nodes, links) {
@@ -50,15 +91,15 @@
 		const saveData = {nodes, links: saveLinks};
 		console.log('saved to localStorage', saveData);
 		let jsonStr = JSON.stringify(saveData);
-		localStorage.setItem(role, jsonStr);
+		window.localStorage.setItem(role, jsonStr);
 	}
-
 
 
 </script>
 
 
 <div class="localstorage container">
+	<div class="mt-n1 container">
 	<button class="btn btn-primary delete-localstorage"
 		role="button"
 		on:click={deleteLocalStorage}>
@@ -69,9 +110,20 @@
 
 		Show localstorage
 	</button>
+	</div>
+	
+	<div class="row">
+		<div class="col">
+		{#each roles as r}
+			<input class="btn btn-primary" type="button" value={r}>
+		{/each}
+		</div>
+	</div>
 	
 	<h1>Here be elements
-			<form on:submit|preventDefault={() => loadLocalStorage(role)}>
+			<form on:submit|preventDefault={() => {
+				saveRole(role, roleData);
+			}}>
 			<div class="input-group mb-3">
 					<span class="input-group-text" id="basic-addon1">Class</span>
 					<input bind:value={role} type="text" class="form-control" placeholder="{role}" aria-label="Username" aria-describedby="basic-addon1">
