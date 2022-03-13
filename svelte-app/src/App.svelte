5bbbae910c909ba0cb2ead2350fae3b8ad793512
Kirishima <97999610+water2thewave@users.noreply.github.com>
2022-03-12 21:57:35 -0500

    huh

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -3,6 +3,7 @@
 	export let name;
 
 	import KnowledgeGraph from './KnowledgeGraph.svelte';
+	var role = "wizard";
 
 	function deleteLocalStorage() {
 		window.localStorage.clear();
@@ -24,7 +25,7 @@
 	<div class="collapse" id="lscontent">
 		<div class="card cardbody">
 			<label for="edit-json"> 'wizard' </label>
-			<textarea class="edit-json"> dummy </textarea>
+			<textarea class="edit-json"> { window.localStorage.getItem(role) } </textarea>
 			<a class="copy-json btn btn-primary"> Copy text </a> 
 		</div>
 	</div>
